e6646dc821b3358e2c37ff42ab2d7c51919988c1
Jovanni G <bugsworld85@gmail.com>
2021-02-22 17:28:21 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import NeoTable from "./NeoTable";
 import NeoPaginator from "./components/Paginator";
+import NeoWindow from "./components/Window";
 
 export default {
     install(Vue) {
         Vue.component("neo-table", NeoTable);
         Vue.component("neo-paginator", NeoPaginator);
+        Vue.component("neo-window", NeoWindow);
     },
-    NeoTable: NeoTable,
-    NeoPaginator: NeoPaginator,
+    NeoTable,
+    NeoPaginator,
+    NeoWindow,
 };
 
 // if (typeof window !== "undefined" && window.Vue) {
